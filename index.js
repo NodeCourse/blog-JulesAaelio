0d181eee31a9e8d672c03f08965eadab16008b04
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 09:15:29 +0200

    init db conn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const sequelize = require('sequelize');
 const app = new express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+
+const db = new sequelize('nodetest','datauser','toto',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+db.authenticate().then(r => {
+    console.log(r);
+}).catch(e => {
+    console.log(e);
+});
 
 console.log("App started at " , new Date().toLocaleString());
 
