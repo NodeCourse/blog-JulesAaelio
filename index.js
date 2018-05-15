4a3f64022faf18851cf07b6d9980d24ac6d7ca18
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 09:50:31 +0200

    article creation : ok

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const db = require('./database');
 const app = new express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 console.log("App started at " , new Date().toLocaleString());
 
-app.use((req,res) => {
+app.get('/',(req,res) => {
     res.render('index');
 });
 
-
-
-db.Article.sync().then(()=> {
-    return db.Article.findAll().then(r => {
-        console.log(r);
-    });
-}).then(r =>{
-    console.log(r);
-}).then(r => {
-    return db.Article.create({
-       title:'test',
-       content:'testee',
-    });
+app.post('/form_handle',(req,res) => {
+    if(req.body && req.body.title && req.body.content) {
+        db.Article
+            .sync()
+            .then(() => {
+                db.Article.create({
+                    title: req.body.title,
+                    content: req.body.content
+                }).then(r => {
+                    console.log('Article created')
+                }).catch(e => {
+                    console.log("ERROR");
+                })
+            });
+    }
+    res.send('Test');
 });
-
 
 
 app.listen(3000);
