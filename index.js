b3624d93b9b60ce0febfdd33f2d66e1ee0e60c1c
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 08:56:55 +0200

    express + pug init

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = new express();
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
 console.log("App started at " , new Date().toLocaleString());
+
+app.use((req,res) => {
+    res.render('index');
+});
+
+app.listen(3000);
