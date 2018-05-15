99d401b765eaa0f6a2d0b2a6a5e3db5d264eb6a9
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 14:52:05 +0200

    displaying and ordering by votes

--- index.js
+++ index.js
@@ -19,6 +19,30 @@ app.get('/',(req,res) => {
     displayAll(res);
 });
 
+app.post('/vote/up/:id',(req,res) => {
+   db.Article.findById(req.params.id).then((o) => {
+       o.increment('upvotes');
+   }).then(r => {
+       console.log('Upvoted');
+       res.sendStatus(204);
+   }).catch(e => {
+       console.log('error upvoting');
+       console.log(e);
+   });
+});
+
+app.post('/vote/down/:id',(req,res) => {
+    db.Article.findById(req.params.id).then((o) => {
+        o.increment('downvotes');
+    }).then(r => {
+        console.log('Downvoted');
+        res.sendStatus(204);
+    }).catch(e => {
+        console.log('error downvoting');
+        console.log(e);
+    });
+});
+
 app.post('/form_handle',(req,res) => {
     if(req.body && req.body.title && req.body.content) {
         db.Article
@@ -41,7 +65,9 @@ app.post('/form_handle',(req,res) => {
 function displayAll(res) {
     return db.Article.sync()
         .then(() => {
-            db.Article.findAll().then(r => {
+            db.Article.findAll({
+                order: [db.db.literal('(upvotes - downvotes) DESC') ]
+            }).then(r => {
                 res.render('list', {
                     articles: r
                 });
